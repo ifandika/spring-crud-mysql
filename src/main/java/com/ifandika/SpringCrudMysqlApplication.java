7ec8966c3a326ec689d716e3bab93dd0562d970f
ifandika <ifandika014@gmail.com>
2024-07-01 04:27:59 +0700

    First init project

---
+++ src/main/java/com/ifandika/SpringCrudMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.ifandika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudMysqlApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ifandika/spring_crud_mysql/SpringCrudMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifandika.spring_crud_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
